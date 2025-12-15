4fe89a4eb1d264b9d16f3b537749ec74ffdb4e59
Shreya Nakum <shreyanakum2@gmail.com>
2025-12-14 21:57:55 -0800

    gemini embeddings api exploration

---
+++ src/1.DomainEmbeddingSpace/Noodling/gemini.py
@@ -0,0 +1,29 @@
+from google import genai
+from google.genai import types
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+client = genai.Client()
+
+texts = [
+    "What is the meaning of life?",
+    "What is the purpose of existence?",
+    "How do I bake a cake?"]
+
+result = [
+    np.array(e.values) for e in client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=texts,
+        config=types.EmbedContentConfig(task_type="CLUSTERING")).embeddings
+]
+
+# Calculate cosine similarity. Higher scores = greater semantic similarity.
+
+embeddings_matrix = np.array(result)
+similarity_matrix = cosine_similarity(embeddings_matrix)
+
+for i, text1 in enumerate(texts):
+    for j in range(i + 1, len(texts)):
+        text2 = texts[j]
+        similarity = similarity_matrix[i, j]
+        print(f"Similarity between '{text1}' and '{text2}': {similarity:.4f}")
